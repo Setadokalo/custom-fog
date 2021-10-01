ed71bad7267085de5d12fff831888e22048f5888
Setadokalo <darkenchanter1@gmail.com>
2021-10-01 08:05:23 -0500

    Change V. Options mixin for BSVB compat, bump version

--- src/main/java/setadokalo/customfog/mixin/VideoOptionsMixin.java
+++ src/main/java/setadokalo/customfog/mixin/VideoOptionsMixin.java
@@ -1,5 +1,6 @@
 package setadokalo.customfog.mixin;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +15,8 @@ import net.minecraft.text.TranslatableText;
 import setadokalo.customfog.CustomFog;
 import setadokalo.customfog.CustomFogClient;
 
-@Mixin(VideoOptionsScreen.class)
+// priority 1100 so we can fail-soft with BSVBP
+@Mixin(value = VideoOptionsScreen.class, priority = 900)
 public abstract class VideoOptionsMixin extends GameOptionsScreen {
 
 	private VideoOptionsMixin() {
@@ -23,20 +25,22 @@ public abstract class VideoOptionsMixin extends GameOptionsScreen {
 
 	@Inject(method = "init", at = @At(value = "RETURN"))
 	protected void addCustomFogButton(CallbackInfo ci) {
-		if (CustomFogClient.config.videoOptionsButton && client != null)
+		if (!FabricLoader.getInstance().isModLoaded("bettersodiumvideosettingsbutton") &&
+				CustomFogClient.config.videoOptionsButton && client != null) {
 			this.addDrawableChild(
 				new ButtonWidget(
-					this.width - 108, 
-					this.height - 27, 
-					100, 
-					20, 
-					new TranslatableText("button.customfog.menu"), 
+					this.width - 108,
+					this.height - 27,
+					100,
+					20,
+					new TranslatableText("button.customfog.menu"),
 					btn -> client.openScreen(CustomFog.getConfigScreen(this))
 				)
 			);
+		}
 	} 
 
-	@ModifyConstant(method = "init", constant = @Constant(intValue = 100))
+	@ModifyConstant(method = "init", constant = @Constant(intValue = 100), require = 0)
 	protected int fixDoneButtonPos(int curVal) {
 		int maxBStart = width - 316;
 		int curBStart = (width / 2) - curVal;
